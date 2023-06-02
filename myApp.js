5d088202952d5aedb16649950f6eb742f515ff76
Praise Freedom Dike <70984186+freedompraise@users.noreply.github.com>
2023-06-02 06:36:59 -0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
-
+app.get('/', (req, res) => {
+  console.log('Hello World');
+  res.send('Hello World');
+});
 
 
 
