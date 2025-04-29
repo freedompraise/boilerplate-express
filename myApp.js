10bc79c226f119b359b5e9b4cda592e8b02ac908
freedompraise <dikepraise119@gmail.com>
2025-04-29 23:46:30 +0100

    Enable watch mode for server script in package.json

--- myApp.js
+++ myApp.js
@@ -1,42 +1,4 @@
 let express = require('express');
 let app = express();
-app.get('/', (req, res) => {
-  console.log('Hello World');
-  res.send('Hello World');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+console.log('Hello World');
+module.exports = app;
